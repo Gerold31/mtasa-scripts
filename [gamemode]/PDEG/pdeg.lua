249153b07ed2b371eddfb9221c2117439e3f95ee
Gerold31 <kramer.leroy@gmail.com>
2015-05-07 18:06:32 +0200

    add basic gamemode

---
+++ [gamemode]/PDEG/pdeg.lua
@@ -0,0 +1,22 @@
+﻿local posX, posY, posZ = 2485, -1667, 13.3
+
+function spawn(player)
+	if not isElement(player) then return end
+
+	spawnPlayer(player, posX, posY, posZ)
+	fadeCamera(player, true)
+	setCameraTarget(player, player)
+	showChat(player, true)
+end
+
+addEventHandler("onPlayerJoin", getRootElement(),
+	function()
+		spawn(source)
+	end
+)
+
+addEventHandler("onPlayerWasted", getRootElement(),
+	function()
+		setTimer(spawn, 1800, 1, source)
+	end
+)
