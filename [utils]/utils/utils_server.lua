20c891346922deba1ac8823a5c31229090269614
Gerold31 <kramer.leroy@gmail.com>
2015-05-09 18:30:40 +0200

    add utils_shared script

--- [utils]/utils/utils_server.lua
+++ [utils]/utils/utils_server.lua
@@ -1,19 +1,5 @@
 addEvent("onVehicleSpawn")
 
-
-function getVehicleHandlingProperty ( element, property )
-	if isElement ( element ) and getElementType ( element ) == "vehicle" and type ( property ) == "string" then -- Make sure there's a valid vehicle and a property string
-		local handlingTable = getVehicleHandling ( element ) -- Get the handling as table and save as handlingTable
-		local value = handlingTable[property] -- Get the value from the table
-
-		if value then -- If there's a value (valid property)
-			return value -- Return it
-		end
-	end
-
-	return false -- Not an element, not a vehicle or no valid property string. Return failure
-end
-
 function spawnVehicle(id, x, y, z)
 	veh = createVehicle(id,x,y,z)
 	if(veh) then
