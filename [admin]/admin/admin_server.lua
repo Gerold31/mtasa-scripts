ff7549218758c20db57ff59a50914f3a2d003da6
Gerold31 <kramer.leroy@gmail.com>
2015-05-09 18:25:40 +0200

    add pos and spawnAll command

--- [admin]/admin/admin_server.lua
+++ [admin]/admin/admin_server.lua
@@ -1,5 +1,9 @@
 -- @todo restric access
 
+vehicleMass = {}
+
+local vehicleIDs = exports["utils"]:getVehicleIDs();
+
 function getVehicleHandlingProperty(vehid, prop)
 	return call(getResourceFromName("utils"), "getVehicleHandlingProperty", vehid, prop)
 end
@@ -7,6 +11,17 @@ end
 function spawnVehicle(vehid, x,y,z)
 	return call(getResourceFromName("utils"), "spawnVehicle", vehid, x,y,z)
 end
+
+for i, id in pairs(vehicleIDs) do
+	local mass = getVehicleHandlingProperty(id, "mass")
+	vehicleMass[i] = {id, mass};
+end
+
+table.sort(vehicleMass,
+function(a,b)
+	return a[2] < b[2]
+end
+)
 
 -- set Vehicle Handling property
 function setVH(thePlayer, command, prop, val)
@@ -60,3 +75,29 @@ function createWeapon(thePlayer, command, weaponid)
 	giveWeapon(thePlayer, weaponid, 500)
 end
 addCommandHandler("cw", createWeapon)
+
+-- prints current position in chat
+function printPos(thePlayer, command)
+	local x,y,z = getElementPosition(thePlayer)
+	outputChatBox("Pos: " .. round(x, 2) .. "," .. round(y, 2) .. "," .. round(z, 2), thePlayer)
+end
+addCommandHandler("pos", printPos)
+
+-- spawns all Vehicles at LS International
+function spawnAll(thePlayer, command)
+	local x,y,z = 1410, -2494, 15
+	setElementPosition(thePlayer, x,y,z)
+	for _, id in pairs(vehicleMass) do
+		if(getVehicleType(id[1]) == "Automobile") then
+			spawnVehicle(id[1],x,y,z)
+			x = x + 5
+			if(x>=2070) then x, y = 1410, -2593 end
+		end
+	end
+end
+addCommandHandler("spawnAll", spawnAll)
+
+function round(num, idp)
+  local mult = 10^(idp or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
