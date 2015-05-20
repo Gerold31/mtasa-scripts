6b59b2632dcd5aa58a7277ff1f87635f87756f60
Gerold31 <kramer.leroy@gmail.com>
2015-05-20 23:40:10 +0200

    add attachpoints for cars

--- [gameplay]/vehicles/attach_server.lua
+++ [gameplay]/vehicles/attach_server.lua
@@ -36,6 +36,20 @@ local mountPoints = { [0] = {
 	{ 0.8,-5.5, 0.23, 15,0,0}
 } }
 
+local vehicleSize = {
+ [581] = 1, [510] = 1, [509] = 1, [522] = 1, [481] = 1, [461] = 1, [462] = 1, [448] = 1, [521] = 1, [468] = 1, [463] = 1, [586] = 1,
+ [523] = 1, [441] = 1, [594] = 1, [501] = 1, [564] = 1, [471] = 1,
+ [572] = 2, [464] = 2, [571] = 2, [610] = 2, [465] = 2,
+ [602] = 4, [545] = 4, [496] = 4, [517] = 4, [401] = 4, [410] = 4, [518] = 4, [600] = 4, [527] = 4, [436] = 4, [589] = 4, [580] = 4,
+ [419] = 4, [439] = 4, [533] = 4, [549] = 4, [526] = 4, [491] = 4, [474] = 4, [445] = 4, [426] = 4, [507] = 4, [547] = 4, [585] = 4,
+ [405] = 4, [587] = 4, [550] = 4, [492] = 4, [566] = 4, [546] = 4, [540] = 4, [551] = 4, [421] = 4, [516] = 4, [529] = 4, [438] = 4,
+ [574] = 4, [420] = 4, [596] = 4, [597] = 4, [598] = 4, [531] = 4, [543] = 4, [422] = 4, [478] = 4, [605] = 4, [536] = 4, [575] = 4,
+ [535] = 4, [576] = 4, [402] = 4, [542] = 4, [603] = 4, [475] = 4, [568] = 4, [424] = 4, [504] = 4, [539] = 4, [429] = 4, [411] = 4,
+ [541] = 4, [559] = 4, [415] = 4, [561] = 4, [480] = 4, [560] = 4, [562] = 4, [506] = 4, [565] = 4, [451] = 4, [434] = 4, [558] = 4,
+ [494] = 4, [555] = 4, [502] = 4, [477] = 4, [503] = 4, [404] = 4, [479] = 4, [458] = 4, [606] = 4, [607] = 4, [611] = 4, [485] = 4,
+ [409] = 6, [534] = 6, [412] = 6, [467] = 6, [604] = 6, [466] = 6,
+}
+
 local rampState = {} -- 0: ramp down, 1: ramp up
 
 local mountedVehicles = {}
@@ -122,16 +136,66 @@ function movePackerRamp(vehicle, pos)
 
 	triggerClientEvent(getVehicleOccupant(vehicle, 0), "onPackerRampMove", vehicle, (1-pos) * rampDown + pos * rampUp)
 
-	for s, child in pairs(mountedVehicles[vehicle]) do
-		if(s > slotsUp) then break end
+	local off = 0;
+	for s=1,slotsUp do
+		if(s+off > slotsUp) then break end
+		local child = mountedVehicles[vehicle][s+off]
 		if(child) then
-			local px = (1-pos) * mountPoints[0][s][1] + pos * mountPoints[1][s][1]
-			local py = (1-pos) * mountPoints[0][s][2] + pos * mountPoints[1][s][2]
-			local pz = (1-pos) * mountPoints[0][s][3] + pos * mountPoints[1][s][3]
-			local rx = (1-pos) * mountPoints[0][s][4] + pos * mountPoints[1][s][4]
-			local ry = (1-pos) * mountPoints[0][s][5] + pos * mountPoints[1][s][5]
-			local rz = (1-pos) * mountPoints[0][s][6] + pos * mountPoints[1][s][6]
+			local size = vehicleSize[getElementModel(child)]
+			local px = 0
+			local py = 0
+			local pz = 0
+			local rx = 0
+			local ry = 0
+			local rz = 0
+			for i=0,size-1 do
+				px = px + (1-pos) * mountPoints[0][s+i+off][1] + pos * mountPoints[1][s+i+off][1]
+				py = py + (1-pos) * mountPoints[0][s+i+off][2] + pos * mountPoints[1][s+i+off][2]
+				pz = pz + (1-pos) * mountPoints[0][s+i+off][3] + pos * mountPoints[1][s+i+off][3]
+				rx = rx + (1-pos) * mountPoints[0][s+i+off][4] + pos * mountPoints[1][s+i+off][4]
+				ry = ry + (1-pos) * mountPoints[0][s+i+off][5] + pos * mountPoints[1][s+i+off][5]
+				rz = rz + (1-pos) * mountPoints[0][s+i+off][6] + pos * mountPoints[1][s+i+off][6]
+			end
+			px = px	/ size
+			py = py	/ size
+			pz = pz	/ size
+			rx = rx	/ size
+			ry = ry	/ size
+			rz = rz	/ size
 			setElementAttachedOffsets(child, px,py,pz, rx,ry,rz)
+			off = off + size - 1
+		end
+	end
+end
+
+function getNextFreeSlot(vehicle, size)
+	if(size == 1) then
+		for i=1,slotsUp+slotsDown do
+			if(not mountedVehicles[vehicle][i]) then
+				return i;
+			end
+		end
+	else
+		local i
+		for i=1,slotsUp-size+1,2 do
+			local accepted = true
+			for j=0,size-1 do
+				if(mountedVehicles[vehicle][i+j]) then
+					accepted = false
+					break
+				end
+			end
+			if(accepted) then return i end
+		end
+		for i=slotsUp+1,slotsUp+slotsDown-size+1,2 do
+			local accepted = true
+			for j=0,size-1 do
+				if(mountedVehicles[vehicle][i+j]) then
+					accepted = false
+					break
+				end
+			end
+			if(accepted) then return i end
 		end
 	end
 end
@@ -139,22 +203,25 @@ end
 function mountVehicle(player, command, vehicleID, slot)
 	vehicleID = tonumber(vehicleID) or 522
 	slot = tonumber(slot) or -1
+	local size = vehicleSize[vehicleID]
+	if(not size) then
+		outputChatBox("This vehicle can't be mounted.")
+		return
+	end
 
 	local vehicle = getPedOccupiedVehicle(player)
 
 	if(vehicle and getElementModel(vehicle) == packerID) then
 		if(slot < 1) then
-			for i=1,slotsUp+slotsDown do
-				if(not mountedVehicles[vehicle][i]) then
-					slot = i
-					break;
-				end
-			end
+			slot = getNextFreeSlot(vehicle, size)
 		end
 
-		if(mountedVehicles[vehicle] and mountedVehicles[vehicle][slot]) then
-			outputChatBox("There is already a vehicle in this slot", player)
-			return
+
+		for i=0,size-1 do
+			if(mountedVehicles[vehicle][slot+i]) then
+				outputChatBox("There is already a vehicle in this slot", player)
+				return
+			end
 		end
 
 		if(slot < 1 or slot > slotsUp+slotsDown) then
@@ -164,38 +231,49 @@ function mountVehicle(player, command, vehicleID, slot)
 
 		local pos = rampState[vehicle]
 
-		local px
-		local py
-		local pz
-		local rx
-		local ry
-		local rz
+		local px = 0
+		local py = 0
+		local pz = 0
+		local rx = 0
+		local ry = 0
+		local rz = 0
 
 		if(slot > slotsUp) then
 			if(pos > 1-1/animationSteps) then
-				px = mountPoints[1][slot][1]
-				py = mountPoints[1][slot][2]
-				pz = mountPoints[1][slot][3]
-				rx = mountPoints[1][slot][4]
-				ry = mountPoints[1][slot][5]
-				rz = mountPoints[1][slot][6]
+				for i=0,size-1 do
+					px = px + mountPoints[1][slot+i][1]
+					py = py + mountPoints[1][slot+i][2]
+					pz = pz + mountPoints[1][slot+i][3]
+					rx = rx + mountPoints[1][slot+i][4]
+					ry = ry + mountPoints[1][slot+i][5]
+					rz = rz + mountPoints[1][slot+i][6]
+				end
 			else
 				outputChatBox("Move the ramp up first", player)
 				return
 			end
 		else
 			if(pos < 1/animationSteps) then
-				px = (1.-pos) * mountPoints[0][slot][1] + pos * mountPoints[1][slot][1]
-				py = (1.-pos) * mountPoints[0][slot][2] + pos * mountPoints[1][slot][2]
-				pz = (1.-pos) * mountPoints[0][slot][3] + pos * mountPoints[1][slot][3]
-				rx = (1.-pos) * mountPoints[0][slot][4] + pos * mountPoints[1][slot][4]
-				ry = (1.-pos) * mountPoints[0][slot][5] + pos * mountPoints[1][slot][5]
-				rz = (1.-pos) * mountPoints[0][slot][6] + pos * mountPoints[1][slot][6]
+				for i=0,size-1 do
+					px = px + mountPoints[0][slot+i][1]
+					py = py + mountPoints[0][slot+i][2]
+					pz = pz + mountPoints[0][slot+i][3]
+					rx = rx + mountPoints[0][slot+i][4]
+					ry = ry + mountPoints[0][slot+i][5]
+					rz = rz + mountPoints[0][slot+i][6]
+				end
 			else
 				outputChatBox("Move the ramp down first", player)
 				return
 			end
 		end
+
+		px = px / size
+		py = py / size
+		pz = pz / size
+		rx = rx / size
+		ry = ry / size
+		rz = rz / size
 
 		x,y,z = getElementPosition(vehicle)
 
@@ -204,7 +282,10 @@ function mountVehicle(player, command, vehicleID, slot)
 		attachElements(v, vehicle, px,py,pz, rx,ry,rz)
 
 		if(not mountedVehicles[vehicle]) then mountedVehicles[vehicle] = {} end
-		mountedVehicles[vehicle][slot] = v
+
+		for i=0,size-1 do
+			mountedVehicles[vehicle][slot+i] = v
+		end
 	else
 		outputChatBox("You have to be in a Packer", player)
 	end
