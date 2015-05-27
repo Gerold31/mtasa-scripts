2433a433fc304bebeb6627c3dd62e46d5ac95133
Gerold31 <kramer.leroy@gmail.com>
2015-05-27 11:40:05 +0200

    save vehicle damage

--- [gameplay]/vehicles/management_server.lua
+++ [gameplay]/vehicles/management_server.lua
@@ -9,17 +9,20 @@ function initResource()
 		cancelEvent()
 		return
 	end
-	dbExec(dbConnection, "CREATE TABLE IF NOT EXISTS vehicles(id INTEGER PRIMARY KEY, vehicleID INTEGER, posX REAL, posY REAL, posZ REAL, rotX REAL, rotY REAL, rotZ REAL, spawned INTEGER)")
+	dbExec(dbConnection, "CREATE TABLE IF NOT EXISTS vehicles(id INTEGER PRIMARY KEY, vehicleID INTEGER, posX REAL, posY REAL, posZ REAL, rotX REAL, rotY REAL, rotZ REAL, spawned INTEGER, " ..
+		   "health INTEGER, isDamageProof INTEGER, panel0 INTEGER, panel1 INTEGER, panel2 INTEGER, panel3 INTEGER, panel4 INTEGER, panel5 INTEGER, panel6 INTEGER)")
 	dbQuery(
 		function(qh)
 			local result = dbPoll(qh, 0)
 			if result then
 				for _, row in ipairs(result) do
-					local vehicle = spawnVehicle(row["vehicleID"], row["posX"], row["posY"], row["posZ"], row["rotX"], row["rotY"], row["rotZ"])
-					setElementData(vehicle, "id", row["id"], false)
+					local vehicle = spawnVehicle(row)
+					if(not vehicle) then
+						outputServerLog("failed to Spawn vehicle")
+					end
 				end
 			end
-		 end, dbConnection, "SELECT id, vehicleID, posX, posY, posZ, rotX, rotY, rotZ FROM `vehicles` WHERE spawned == 1")
+		 end, dbConnection, "SELECT * FROM `vehicles` WHERE spawned == 1")
 end
 addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), initResource)
 
@@ -30,9 +33,7 @@ function stopResource()
 		if(not id) then
 			ouputServerLog("Vehicle has no id!")
 		else
-			x,y,z = getElementPosition(vehicle)
-			rx,ry,rz = getElementRotation(vehicle)
-			dbExec(dbConnection, "UPDATE vehicles SET posX=?, posY=?, posZ=?, rotX=?, rotY=?, rotZ=? WHERE id=?", x,y,z, rx,ry,rz, id)
+			saveVehicle(vehicle)
 		end
 	end
 end
@@ -40,7 +41,10 @@ addEventHandler("onResourceStop", getResourceRootElement(getThisResource()), sto
 
 function create(id, x, y, z)
 	-- @todo verify id
-	local qh = dbQuery(dbConnection, "INSERT INTO vehicles(vehicleID, posX, posY, posZ, rotX, rotY, rotZ, spawned) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", id, x, y, z, 0, 0, 0, 0)
+	local qh = dbQuery(dbConnection, "INSERT INTO vehicles(vehicleID, posX, posY, posZ, rotX, rotY, rotZ, spawned, " ..
+					   "health, isDamageProof, panel0, panel1, panel2, panel3, panel4, panel5, panel6) " ..
+					   "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+					   id, x, y, z, 0, 0, 0, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0)
 	local _, _, vid = dbPoll(qh, -1)
 	return tostring(vid)
 end
@@ -55,9 +59,8 @@ function spawn(id, player)
 					if(row["spawned"] == 1) then
 						outputChatBox("Vehicle is already spawned", player)
 					else
-						local vehicle = spawnVehicle(row["vehicleID"], row["posX"], row["posY"], row["posZ"], row["rotX"], row["rotY"], row["rotZ"])
+						local vehicle = spawnVehicle(row)
 						if(vehicle) then
-							setElementData(vehicle, "id", row["id"], false)
 							dbExec(dbConnection, "UPDATE vehicles SET spawned='1' WHERE id=?", row["id"])
 						else
 							outputChatBox("Cannot spawn Vehicle", player)
@@ -68,39 +71,61 @@ function spawn(id, player)
 					outputChatBox("Vehicle doesn't exist", player)
 				end
 			end
-		end, dbConnection, "SELECT id, vehicleID, posX, posY, posZ, rotX, rotY, rotZ FROM `vehicles` WHERE id == ?", id)
+		end, dbConnection, "SELECT * FROM `vehicles` WHERE id == ?", id)
 end
 
 function despawn(id, player)
 	for _, vehicle in pairs(getElementsByType('vehicle')) do
 		if(getElementData(vehicle, "id") == id) then
-			x,y,z = getElementPosition(vehicle)
-			rx,ry,rz = getElementRotation(vehicle)
+			saveVehicle(vehicle)
 			destroyElement(vehicle)
-			dbExec(dbConnection, "UPDATE vehicles SET posX=?, posY=?, posZ=?, rotX=?, rotY=?, rotZ=?, spawned='0' WHERE id=?", x,y,z, rx,ry,rz, id)
+			dbExec(dbConnection, "UPDATE vehicles SET spawned='0' WHERE id=?", id)
 			return
 		end
 	end
 	outputChatBox("Vehicle isn't spawned or doesn't exist", player)
 end
 
-function spawnVehicle(id, x, y, z, rx, ry, rz)
-	local def = getVehicleDefinition(id)
+function spawnVehicle(row)
+	local def = getVehicleDefinition(row["vehicleID"])
 	if(def) then
-		local veh = createVehicle(id, x,y,z, rx,ry,rz)
-		if(veh) then
+		local vehicle = createVehicle(row["vehicleID"], row["posX"],row["posY"],row["posZ"], row["rotX"],row["rotZ"],row["rotZ"])
+		if(vehicle) then
+			setElementData(vehicle, "id", row["id"], false)
+
+			setElementHealth(vehicle, row["health"])
+			setVehicleDamageProof(vehicle, row["isDamageProof"] == 1 and true or false)
+			for i=0,6 do
+				setVehiclePanelState(vehicle, i, row["panel" .. i])
+			end
+
+			-- inventory
 			local invs = getVehicleDefinitionInventories(def)
 			for key, vol in pairs(invs) do
-				exports["inventory"]:addInventory(veh, key, vol)
+				exports["inventory"]:addInventory(vehicle, key, vol)
 				-- debug
 				--local item = exports["inventory"]:getItemDefinition(1)
 				--local succ = exports["inventory"]:addInventoryItem(exports["inventory"]:getInventory(veh, key), item, 1000)
 				--if(not succ) then outputServerLog("failed") end
 			end
-			triggerEvent("onVehicleSpawn", veh)
+			triggerEvent("onVehicleSpawn", vehicle)
 		end
-		return veh
+		return vehicle
 	end
 	return nil
 end
 
+function saveVehicle(vehicle)
+	local id = getElementData(vehicle, "id")
+	local x,y,z = getElementPosition(vehicle)
+	local rx,ry,rz = getElementRotation(vehicle)
+	local health = getElementHealth(vehicle)
+	local isDamageProof = isVehicleDamageProof(vehicle) and 1 or 0
+	local panel = {}
+	for i=0,6 do
+		panel[i] = getVehiclePanelState(vehicle, i)
+	end
+	dbExec(dbConnection, "UPDATE vehicles SET posX=?, posY=?, posZ=?, rotX=?, rotY=?, rotZ=?, " ..
+		   "health=?, isDamageProof=?, panel0=?, panel1=?, panel2=?, panel3=?, panel4=?, panel5=?, panel6=? WHERE id=?",
+		   x,y,z, rx,ry,rz, health, isDamageProof, panel[0], panel[1], panel[2], panel[3], panel[4], panel[5], panel[6], id)
+end
