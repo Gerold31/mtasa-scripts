f360550e8fb207d5fb051673d8a3f24052bcfb49
Gerold31 <kramer.leroy@gmail.com>
2015-05-31 18:38:04 +0200

    save vehicle color

--- [gameplay]/vehicles/management_server.lua
+++ [gameplay]/vehicles/management_server.lua
@@ -1,3 +1,6 @@
+-- @todo save paintjob, upgrades, plate, variant, enginestate, lightstate (including taxi and siren), door locked
+-- inventory, fuel
+
 local dbConnection = nil
 
 addEvent("onVehicleSpawn")
@@ -12,7 +15,8 @@ function initResource()
 	dbExec(dbConnection, "CREATE TABLE IF NOT EXISTS vehicles(id INTEGER PRIMARY KEY, vehicleID INTEGER, posX REAL, posY REAL, posZ REAL, rotX REAL, rotY REAL, rotZ REAL, spawned INTEGER, " ..
 		   "health INTEGER, isDamageProof INTEGER, panel0 INTEGER, panel1 INTEGER, panel2 INTEGER, panel3 INTEGER, panel4 INTEGER, panel5 INTEGER, panel6 INTEGER, " ..
 		   "door0 INTEGER, door1 INTEGER, door2 INTEGER, door3 INTEGER, door4 INTEGER, door5 INTEGER, doorR0 REAL, doorR1 REAL, doorR2 REAL, doorR3 REAL, doorR4 REAL, doorR5 REAL, " ..
-		   "wheel0 INTEGER, wheel1 INTEGER, wheel2 INTEGER, wheel3 INTEGER)")
+		   "wheel0 INTEGER, wheel1 INTEGER, wheel2 INTEGER, wheel3 INTEGER, " ..
+		   "color0r INTEGER, color0g INTEGER, color0b INTEGER, color1r INTEGER, color1g INTEGER, color1b INTEGER, color2r INTEGER, color2g INTEGER, color2b INTEGER, color3r INTEGER, color3g INTEGER, color3b INTEGER)")
 	dbQuery(
 		function(qh)
 			local result = dbPoll(qh, 0)
@@ -44,13 +48,16 @@ addEventHandler("onResourceStop", getResourceRootElement(getThisResource()), sto
 function create(id, x, y, z)
 	local def = getVehicleDefinition(id)
 	if(not def) then return nil end
+	local r = math.random
 	local qh = dbQuery(dbConnection, "INSERT INTO vehicles(vehicleID, posX, posY, posZ, rotX, rotY, rotZ, spawned, " ..
 					   "health, isDamageProof, panel0, panel1, panel2, panel3, panel4, panel5, panel6, " ..
 					   "door0, door1, door2, door3, door4, door5, doorR0, doorR1, doorR2, doorR3, doorR4, doorR5, " ..
-					   "wheel0, wheel1, wheel2, wheel3) " ..
+					   "wheel0, wheel1, wheel2, wheel3, " ..
+					   "color0r, color0g, color0b, color1r, color1g, color1b, color2r, color2g, color2b, color3r, color3g, color3b) " ..
 					   "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " ..
-					   "?, ?, ?, ?)",
-					   id, x, y, z, 0, 0, 0, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+					   "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+					   id, x, y, z, 0, 0, 0, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					   r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256))
 	local _, _, vid = dbPoll(qh, -1)
 	return tostring(vid)
 end
@@ -118,6 +125,7 @@ function spawnVehicle(row)
 				setVehicleDoorOpenRatio(vehicle, i, row["doorR" .. i])
 			end
 			setVehicleWheelStates(vehicle, row["wheel0"], row["wheel1"], row["wheel2"], row["wheel3"])
+			setVehicleColor(vehicle, row["color0r"], row["color0g"], row["color0b"], row["color1r"], row["color1g"], row["color1b"], row["color2r"], row["color2g"], row["color2b"], row["color3r"], row["color3g"], row["color3b"])
 
 			-- inventory
 			local invs = getVehicleDefinitionInventories(def)
@@ -145,6 +153,7 @@ function saveVehicle(vehicle)
 	local door = {}
 	local doorR = {}
 	local wheel0, wheel1, wheel2, wheel3 = getVehicleWheelStates(vehicle)
+	local r0, g0, b0, r1, g1, b1, r2, g2, b2, r3, g3, b3 = getVehicleColor(vehicle, true)
 	for i=0,6 do
 		panel[i] = getVehiclePanelState(vehicle, i)
 	end
@@ -155,8 +164,9 @@ function saveVehicle(vehicle)
 	dbExec(dbConnection, "UPDATE vehicles SET posX=?, posY=?, posZ=?, rotX=?, rotY=?, rotZ=?, " ..
 		   "health=?, isDamageProof=?, panel0=?, panel1=?, panel2=?, panel3=?, panel4=?, panel5=?, panel6=?, " ..
 		   "door0=?, door1=?, door2=?, door3=?, door4=?, door5=?, door0=?, doorR1=?, doorR2=?, doorR3=?, doorR4=?, doorR5=?, " ..
-		   "wheel0=?, wheel1=?, wheel2=?, wheel3=? WHERE id=?",
+		   "wheel0=?, wheel1=?, wheel2=?, wheel3=?, " ..
+		   "color0r=?, color0g=?, color0b=?, color1r=?, color1g=?, color1b=?, color2r=?, color2g=?, color2b=?, color3r=?, color3g=?, color3b=? WHERE id=?",
 		   x,y,z, rx,ry,rz, health, isDamageProof, panel[0], panel[1], panel[2], panel[3], panel[4], panel[5], panel[6],
 		   door[0], door[1], door[2], door[3], door[4], door[5], doorR[0], doorR[1], doorR[2], doorR[3], doorR[4], doorR[5],
-		   wheel0, wheel1, wheel2, wheel3, id)
+		   wheel0, wheel1, wheel2, wheel3, r0, g0, b0, r1, g1, b1, r2, g2, b2, r3, g3, b3, id)
 end
