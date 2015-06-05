d364bc767d4390e7d66085bae159ea2173116768
Gerold31 <kramer.leroy@gmail.com>
2015-06-05 23:13:45 +0200

    save vehicle variant

--- [gameplay]/vehicles/management_server.lua
+++ [gameplay]/vehicles/management_server.lua
@@ -1,4 +1,4 @@
--- @todo save upgrades, variant, lightstate (including taxi and siren), door locked
+-- @todo save upgrades, lightstate (including taxi and siren), door locked
 -- inventory, fuel
 
 local dbConnection = nil
@@ -15,7 +15,7 @@ function initResource()
 	dbExec(dbConnection, "CREATE TABLE IF NOT EXISTS vehicles(id INTEGER PRIMARY KEY, vehicleID INTEGER, posX REAL, posY REAL, posZ REAL, rotX REAL, rotY REAL, rotZ REAL, spawned INTEGER, " ..
 		   "health INTEGER, isDamageProof INTEGER, panel0 INTEGER, panel1 INTEGER, panel2 INTEGER, panel3 INTEGER, panel4 INTEGER, panel5 INTEGER, panel6 INTEGER, " ..
 		   "door0 INTEGER, door1 INTEGER, door2 INTEGER, door3 INTEGER, door4 INTEGER, door5 INTEGER, doorR0 REAL, doorR1 REAL, doorR2 REAL, doorR3 REAL, doorR4 REAL, doorR5 REAL, " ..
-		   "wheel0 INTEGER, wheel1 INTEGER, wheel2 INTEGER, wheel3 INTEGER, paintjob INTEGER, " ..
+		   "wheel0 INTEGER, wheel1 INTEGER, wheel2 INTEGER, wheel3 INTEGER, paintjob INTEGER, variant1 INTEGER, variant2 INTEGER, " ..
 		   "color0r INTEGER, color0g INTEGER, color0b INTEGER, color1r INTEGER, color1g INTEGER, color1b INTEGER, color2r INTEGER, color2g INTEGER, color2b INTEGER, color3r INTEGER, color3g INTEGER, color3b INTEGER, " ..
 		   "engine INTEGER, plate TEXT)")
 	dbQuery(
@@ -60,13 +60,13 @@ function create(id, x, y, z)
 	local qh = dbQuery(dbConnection, "INSERT INTO vehicles(vehicleID, posX, posY, posZ, rotX, rotY, rotZ, spawned, " ..
 					   "health, isDamageProof, panel0, panel1, panel2, panel3, panel4, panel5, panel6, " ..
 					   "door0, door1, door2, door3, door4, door5, doorR0, doorR1, doorR2, doorR3, doorR4, doorR5, " ..
-					   "wheel0, wheel1, wheel2, wheel3, paintjob, " ..
+					   "wheel0, wheel1, wheel2, wheel3, paintjob, variant1, variant2, " ..
 					   "color0r, color0g, color0b, color1r, color1g, color1b, color2r, color2g, color2b, color3r, color3g, color3b, " ..
 					   "engine, plate) " ..
 					   "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " ..
-					   "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+					   "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
 					   id, x, y, z, 0, 0, 0, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, r(3),
-					   r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), 0,
+					   -1, -1, r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), 0,
 					   plate)
 	local _, _, vid = dbPoll(qh, -1)
 	return tostring(vid)
@@ -136,6 +136,9 @@ function spawnVehicle(row)
 			end
 			setVehicleWheelStates(vehicle, row["wheel0"], row["wheel1"], row["wheel2"], row["wheel3"])
 			setVehiclePaintjob(vehicle, row["paintjob"])
+			if(row["variant1"] ~= -1 and row["variant2"] ~= -1) then
+				setVehicleVariant(vehicle, row["variant1"], row["variant2"])
+			end
 			setVehicleColor(vehicle, row["color0r"], row["color0g"], row["color0b"], row["color1r"], row["color1g"], row["color1b"], row["color2r"], row["color2g"], row["color2b"], row["color3r"], row["color3g"], row["color3b"])
 			setVehicleEngineState(vehicle, row["engine"] == 1 and true or false)
 			setVehiclePlateText(vehicle, row["plate"])
@@ -167,6 +170,7 @@ function saveVehicle(vehicle)
 	local doorR = {}
 	local wheel0, wheel1, wheel2, wheel3 = getVehicleWheelStates(vehicle)
 	local paintjob = getVehiclePaintjob(vehicle)
+	local var1, var2 = getVehicleVariant(vehicle)
 	local r0, g0, b0, r1, g1, b1, r2, g2, b2, r3, g3, b3 = getVehicleColor(vehicle, true)
 	local engine = getVehicleEngineState(vehicle)
 	local plate = getVehiclePlateText(vehicle)
@@ -180,10 +184,10 @@ function saveVehicle(vehicle)
 	dbExec(dbConnection, "UPDATE vehicles SET posX=?, posY=?, posZ=?, rotX=?, rotY=?, rotZ=?, " ..
 		   "health=?, isDamageProof=?, panel0=?, panel1=?, panel2=?, panel3=?, panel4=?, panel5=?, panel6=?, " ..
 		   "door0=?, door1=?, door2=?, door3=?, door4=?, door5=?, door0=?, doorR1=?, doorR2=?, doorR3=?, doorR4=?, doorR5=?, " ..
-		   "wheel0=?, wheel1=?, wheel2=?, wheel3=?, paintjob=?, " ..
+		   "wheel0=?, wheel1=?, wheel2=?, wheel3=?, paintjob=?, variant1=?, variant2=?, " ..
 		   "color0r=?, color0g=?, color0b=?, color1r=?, color1g=?, color1b=?, color2r=?, color2g=?, color2b=?, color3r=?, color3g=?, color3b=?, " ..
 		   "engine=?, plate=? WHERE id=?",
 		   x,y,z, rx,ry,rz, health, isDamageProof, panel[0], panel[1], panel[2], panel[3], panel[4], panel[5], panel[6],
 		   door[0], door[1], door[2], door[3], door[4], door[5], doorR[0], doorR[1], doorR[2], doorR[3], doorR[4], doorR[5],
-		   wheel0, wheel1, wheel2, wheel3, paintjob, r0, g0, b0, r1, g1, b1, r2, g2, b2, r3, g3, b3, engine, plate, id)
+		   wheel0, wheel1, wheel2, wheel3, paintjob, var1, var2, r0, g0, b0, r1, g1, b1, r2, g2, b2, r3, g3, b3, engine, plate, id)
 end
