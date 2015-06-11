b4b5c6cca73c2972be7103236788b7628fa7fa6a
Gerold31 <kramer.leroy@gmail.com>
2015-06-11 13:37:29 +0200

    save light state

--- [gameplay]/vehicles/management_server.lua
+++ [gameplay]/vehicles/management_server.lua
@@ -1,4 +1,4 @@
--- @todo save upgrades, lightstate (including taxi and siren), door locked
+-- @todo save upgrades, lightoverride (including taxi and siren), door locked
 -- inventory
 
 local dbConnection = nil
@@ -15,7 +15,7 @@ function initResource()
 	dbExec(dbConnection, "CREATE TABLE IF NOT EXISTS vehicles(id INTEGER PRIMARY KEY, vehicleID INTEGER, posX REAL, posY REAL, posZ REAL, rotX REAL, rotY REAL, rotZ REAL, spawned INTEGER, " ..
 		   "health INTEGER, isDamageProof INTEGER, panel0 INTEGER, panel1 INTEGER, panel2 INTEGER, panel3 INTEGER, panel4 INTEGER, panel5 INTEGER, panel6 INTEGER, " ..
 		   "door0 INTEGER, door1 INTEGER, door2 INTEGER, door3 INTEGER, door4 INTEGER, door5 INTEGER, doorR0 REAL, doorR1 REAL, doorR2 REAL, doorR3 REAL, doorR4 REAL, doorR5 REAL, " ..
-		   "wheel0 INTEGER, wheel1 INTEGER, wheel2 INTEGER, wheel3 INTEGER, paintjob INTEGER, variant1 INTEGER, variant2 INTEGER, " ..
+		   "wheel0 INTEGER, wheel1 INTEGER, wheel2 INTEGER, wheel3 INTEGER, light0 INTEGER, light1 INTEGER, light2 INTEGER, light3 INTEGER, paintjob INTEGER, variant1 INTEGER, variant2 INTEGER, " ..
 		   "color0r INTEGER, color0g INTEGER, color0b INTEGER, color1r INTEGER, color1g INTEGER, color1b INTEGER, color2r INTEGER, color2g INTEGER, color2b INTEGER, color3r INTEGER, color3g INTEGER, color3b INTEGER, " ..
 		   "engine INTEGER, plate TEXT, fuel INTEGER)")
 	dbQuery(
@@ -60,13 +60,13 @@ function create(id, x, y, z)
 	local qh = dbQuery(dbConnection, "INSERT INTO vehicles(vehicleID, posX, posY, posZ, rotX, rotY, rotZ, spawned, " ..
 					   "health, isDamageProof, panel0, panel1, panel2, panel3, panel4, panel5, panel6, " ..
 					   "door0, door1, door2, door3, door4, door5, doorR0, doorR1, doorR2, doorR3, doorR4, doorR5, " ..
-					   "wheel0, wheel1, wheel2, wheel3, paintjob, variant1, variant2, " ..
+					   "wheel0, wheel1, wheel2, wheel3, light0, light1, light2, light3, paintjob, variant1, variant2, " ..
 					   "color0r, color0g, color0b, color1r, color1g, color1b, color2r, color2g, color2b, color3r, color3g, color3b, " ..
 					   "engine, plate, fuel) " ..
 					   "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, " ..
-					   "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-					   id, x, y, z, 0, 0, 0, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, r(3),
-					   -1, -1, r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), 0,
+					   "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+					   id, x, y, z, 0, 0, 0, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					   r(3), -1, -1, r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), r(256), 0,
 					   plate, getVehicleDefinitionMaxFuel(def))
 	local _, _, vid = dbPoll(qh, -1)
 	return tostring(vid)
@@ -134,6 +134,9 @@ function spawnVehicle(row)
 				setVehicleDoorState(vehicle, i, row["door" .. i])
 				setVehicleDoorOpenRatio(vehicle, i, row["doorR" .. i])
 			end
+			for i=0,3 do
+				setVehicleLightState(vehicle, i, row["light" .. i])
+			end
 			setVehicleWheelStates(vehicle, row["wheel0"], row["wheel1"], row["wheel2"], row["wheel3"])
 			setVehiclePaintjob(vehicle, row["paintjob"])
 			if(row["variant1"] ~= -1 and row["variant2"] ~= -1) then
@@ -170,6 +173,7 @@ function saveVehicle(vehicle)
 	local door = {}
 	local doorR = {}
 	local wheel0, wheel1, wheel2, wheel3 = getVehicleWheelStates(vehicle)
+	local light = {}
 	local paintjob = getVehiclePaintjob(vehicle)
 	local var1, var2 = getVehicleVariant(vehicle)
 	local r0, g0, b0, r1, g1, b1, r2, g2, b2, r3, g3, b3 = getVehicleColor(vehicle, true)
@@ -183,14 +187,17 @@ function saveVehicle(vehicle)
 		door[i] = getVehicleDoorState(vehicle, i)
 		doorR[i] = getVehicleDoorOpenRatio(vehicle, i)
 	end
+	for i=0,3 do
+		light[i] = getVehicleLightState(vehicle, i)
+	end
 	dbExec(dbConnection, "UPDATE vehicles SET posX=?, posY=?, posZ=?, rotX=?, rotY=?, rotZ=?, " ..
 		   "health=?, isDamageProof=?, panel0=?, panel1=?, panel2=?, panel3=?, panel4=?, panel5=?, panel6=?, " ..
 		   "door0=?, door1=?, door2=?, door3=?, door4=?, door5=?, door0=?, doorR1=?, doorR2=?, doorR3=?, doorR4=?, doorR5=?, " ..
-		   "wheel0=?, wheel1=?, wheel2=?, wheel3=?, paintjob=?, variant1=?, variant2=?, " ..
+		   "wheel0=?, wheel1=?, wheel2=?, wheel3=?, light0=?, light1=?, light2=?, light3=?, paintjob=?, variant1=?, variant2=?, " ..
 		   "color0r=?, color0g=?, color0b=?, color1r=?, color1g=?, color1b=?, color2r=?, color2g=?, color2b=?, color3r=?, color3g=?, color3b=?, " ..
 		   "engine=?, plate=?, fuel=? WHERE id=?",
 		   x,y,z, rx,ry,rz, health, isDamageProof, panel[0], panel[1], panel[2], panel[3], panel[4], panel[5], panel[6],
 		   door[0], door[1], door[2], door[3], door[4], door[5], doorR[0], doorR[1], doorR[2], doorR[3], doorR[4], doorR[5],
-		   wheel0, wheel1, wheel2, wheel3, paintjob, var1, var2, r0, g0, b0, r1, g1, b1, r2, g2, b2, r3, g3, b3, engine, plate,
+		   wheel0, wheel1, wheel2, wheel3, light[0], light[1], light[2], light[3], paintjob, var1, var2, r0, g0, b0, r1, g1, b1, r2, g2, b2, r3, g3, b3, engine, plate,
 		   fuel, id)
 end
